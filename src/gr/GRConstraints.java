94d412c2fefb1e0b096672d0de81e6ab5a8d277a
Davide Cultrera <davidec93@gmail.com>
2015-11-22 23:26:32 +0000

    Implement GRConstraints

--- src/gr/GRConstraints.java
+++ src/gr/GRConstraints.java
@@ -1,5 +1,31 @@
 package gr;
 
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Stores lexical constraints from markedup as "label word" strings in a Set
+ */
 public class GRConstraints {
-    //based on weird _Impl trick
+    //C++ based on  pImpl trick
+
+    private String name;
+    private Set set;
+
+    public GRConstraints(String name) {
+        this.name = name;
+        this.set = new HashSet<>();
+    }
+
+    public String getName() { return name; }
+
+    public int size() { return set.size(); }
+
+    public boolean get(String label, String word) {
+        return set.contains(label + " " + word);
+    }
+
+    public void add(String label, String word) {
+        set.add(label + " " + word);
+    }
 }
