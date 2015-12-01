9106a4b812c1536def66a64406d969664c818d8d
Davide Cultrera <davidec93@gmail.com>
2015-12-01 14:37:11 +0000

    Remove name from GRConstraints

--- src/gr/GRConstraints.java
+++ src/gr/GRConstraints.java
@@ -9,22 +9,15 @@ import java.util.Set;
 public class GRConstraints {
     //C++ based on  pImpl trick
 
-    //FIXME: 'name' is probably useless?
-    private String name;
     private Set<String> set;
 
     public GRConstraints() {
         this.set = new HashSet<String>();
     }
 
-    public GRConstraints(String name) {
-        this.name = name;
-        this.set = new HashSet<String>();
+    public int size() {
+        return set.size();
     }
-
-    public String getName() { return name; }
-
-    public int size() { return set.size(); }
 
     public boolean get(String label, String word) {
         return set.contains(label + " " + word);
