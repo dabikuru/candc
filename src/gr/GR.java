7c9c5d8bd596413258c20560900e67be9be49e0d
Davide Cultrera <davidec93@gmail.com>
2016-03-03 16:37:13 +0000

    Refine compareTo for GR

--- src/gr/GR.java
+++ src/gr/GR.java
@@ -23,12 +23,23 @@ public class GR implements Comparable<GR> {
         return out.toString();
     }
 
+    /**
+     * @param o GR to compare against
+     * @return 1 or -1 if this comes before or after; 0 if they are equal
+     */
     @Override
     public int compareTo(GR o) {
-        if (!this.arguments.isEmpty() && !o.arguments.isEmpty()) {
-            return this.arguments.get(0).pos - o.arguments.get(0).pos;
+        // Comparison for 2-argument dependencies
+        if (this.arguments.size() == 2 && this.arguments.size() == 2) {
+            // First compare the governing words
+            int d1 = this.arguments.get(0).pos - o.arguments.get(0).pos;
+            // If the words are equal (same position), compare the dependent words.
+            // If they are also equal, the whole GR is equal (TreeSet will exclude the second)
+            return (d1 != 0) ? d1 : this.arguments.get(1).pos - o.arguments.get(1).pos;
         }
-        return 0;
+
+        // For deps with more or less than 2 arguments, return default value
+        return 1;
     }
 }
 
